8526107c24f43a15b04a6480c43843b92b0c0455
yogesh4492 <you@example.com>
2025-08-26 21:51:58 +0530

    new file:   Revision/1.cpp 	new file:   Revision/1.exe 	new file:   Revision/2.cpp 	new file:   Revision/2.exe 	new file:   Revision/3.cpp 	new file:   Revision/3.exe 	new file:   Revision/4.cpp 	new file:   Revision/a.exe

---
+++ Revision/2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int roll,std;
+    float percentage=89.98;
+    roll=101;
+    std=12;
+    string name="yogesh";
+    // percentage=89.98;
+    char grade='a';
+    int fees=70000;
+    cout<<"Roll no= "<<roll<<endl;
+    cout<<"Standard= "<<std<<endl;
+    cout<<"percentage= "<<percentage<<endl;
+    cout<<"Name= "<<name<<endl;
+    cout<<"grade= "<<grade<<endl;
+    cout<<"Fees= "<<fees<<endl;
+    return 0;
+}
---
+++ Revision/3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int roll,std;
+    float fees,percentage;
+    char grade;
+    string name;
+    cout<<"Enter roll no= ";
+    cin>>roll;
+    cout<<"enter Your standard= ";
+    cin>>std;
+    cout<<"Enter PErcentage=";
+    cin>>percentage;
+    cout<<"Enter Name= ";
+    cin>>name;
+    cout<<"Enter Grade= ";
+    cin>>grade;
+    cout<<"Enter Fees= ";
+    cin>>fees;
+    cout<<"roll no= "<<roll<<endl;
+    cout<<"Std= "<<std<<endl;
+    cout<<"name= "<<name<<endl;
+    cout<<"Grade= "<<grade<<endl;
+    cout<<"fees="<<fees<<endl;
+    cout<<"percentage= "<<percentage<<endl;
+    return 0;
+}
